c7762d66991c582dcfa0c47056acf9beafa5ec80
Ray Rojas <bichito@hp.com>
2018-05-13 22:56:00 -0500

    ...

---
+++ MyFirstApp/MainActivity.cs
@@ -0,0 +1,35 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using MyFirstApp.Models;
+namespace MyFirstApp
+{
+    [Activity(Label = "MyFirstApp", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        Button btnGuardar;
+        Actividad general = new Actividad();
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+            btnGuardar = FindViewById<Button>(Resource.Id.btnGuardar);
+            btnGuardar.Click += delegate
+            {
+                actionButton();
+            };
+        }
+        private void actionButton()
+        {
+            Actividad o = new Actividad();
+            general.permanence.Add(o.crear_activity());
+            Toast.MakeText(this, "Una actividad fue agregada.", ToastLength.Short).Show();
+            Toast.MakeText(this, "Tienes " + general.permanence.Count + " actividades registradas.", ToastLength.Long).Show();
+
+        }
+    }
+}
+
---
+++ MyFirstApp/Models/Actividad.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace MyFirstApp.Models
+{
+    public class Actividad
+    {
+        public int id { get; set; }
+        public string titulo { get; set; }
+        public string detalle { get; set; }
+
+        public List<Actividad> permanence = new List<Actividad>();
+
+        public Actividad crear_activity()
+        {
+            Actividad o = new Actividad();
+            o.id = 1;
+            o.titulo = "Some";
+            o.detalle = "Descripcion";
+            return o;
+        }
+        public Activity editar_activity(int id, string titulo, string detalle)
+        {
+            return new Activity();
+        }
+    }
+}
